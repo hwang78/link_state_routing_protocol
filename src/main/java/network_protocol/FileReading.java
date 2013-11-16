d425894e4cf2c8c777849d4d97508ca4de22244b
Helen <helenwang789@gmail.com>
2013-11-15 23:49:35 -0600

    option 1 and 2

--- src/main/java/network_protocol/FileReading.java
+++ src/main/java/network_protocol/FileReading.java
@@ -5,14 +5,18 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import org.apache.log4j.Logger;
+
 public class FileReading {
 
+	private final Logger LOGGER = Logger.getLogger(FileReading.class);
 	
 	public ArrayList<String> readFile(String filePath) {
 		if (filePath == null) {
-			System.out.println("Invalid file path.");
+			LOGGER.info("Invalid file path.");
 			return null;
 		}
+		
 		BufferedReader br = null;
 		ArrayList<String> result = new ArrayList<String>();
 		try {
@@ -32,6 +36,7 @@ public class FileReading {
 			}
 		}
 		
+		LOGGER.info("Routing table reads in finished");
 		return result;
 	}
 	
--- src/main/java/network_protocol/Routing.java
+++ src/main/java/network_protocol/Routing.java
@@ -1,5 +1,45 @@
 package network_protocol;
 
+import java.util.ArrayList;
+
+import org.apache.log4j.Logger;
+
 public class Routing {
+	
+	ArrayList<String> orgRoutingTable;
+	FileReading fr = new FileReading();
+	
+	private final Logger LOGGER = Logger.getLogger(Routing.class);
+	
+	public void readsInputRoutingTable(String filePath) {
+		orgRoutingTable = fr.readFile(filePath);
+		if(orgRoutingTable == null) {
+			LOGGER.info("Routing tables read in failed.");
+		}
+		
+		LOGGER.info("Start reads in routing tables...");
+		int tableSize = orgRoutingTable.size();
+		for (int i = 0; i < tableSize; i++) {
+			System.out.print("  R"+(i+1)+" ");
+		}
+		System.out.println();
+		for(int i = 0; i < tableSize; i++) {
+			System.out.println("R" + (i+1)+":" + orgRoutingTable.get(i));
+		}
+		LOGGER.info("Routing table reads in finished");
+	}
+	
+	public void getRouterRoutingTable(int routerNum) {
+		if(orgRoutingTable == null) {
+			LOGGER.info("No routing tables provided");
+			return;
+		}
+		
+		if(routerNum < 1 || routerNum > orgRoutingTable.size()) {
+			LOGGER.info("Router number not exits");
+			return;
+		}
+			
+	}
 
 }
